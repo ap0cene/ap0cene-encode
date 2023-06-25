2393e64809ae3da6344830d47c312ba77f498a9e
Sam Walker <samuel.walker9@gmail.com>
2023-06-25 14:43:33 -0400

    added body layout

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,8 @@ import { Grommet } from 'grommet'
 import { Subtract, Add } from 'grommet-icons'
 import { hpe } from 'grommet-theme-hpe'
 import { deepMerge } from 'grommet/utils'
-import NewProductForm from './components/forms/NewProductForm'
 import PageHeader from './components/PageHeader'
+import NavBody from './components/NavBody'
 
 const customTheme = {
   global: {
@@ -32,7 +32,7 @@ function App() {
   return (
     <Grommet theme={theme}>
       <PageHeader />
-      <NewProductForm />
+      <NavBody />
     </Grommet>
   )
 }
---
+++ src/components/NavBody.tsx
@@ -0,0 +1,34 @@
+import { Box } from 'grommet'
+import styled from 'styled-components'
+import React from 'react'
+import NewProductForm from './forms/NewProductForm'
+
+const BodyContainer = styled(Box)`
+  align-items: center;
+`
+
+const BodyBox = styled(Box)`
+  height: 100%;
+  max-width: 1080px;
+  width: 100%;
+  //border: 1px solid black;
+`
+
+const Container = styled(Box)`
+  position: relative;
+  flex: 1;
+`
+
+function NavBody() {
+  return (
+    <BodyContainer>
+      <BodyBox direction="row">
+        <Container margin="large">
+          <NewProductForm />
+        </Container>
+      </BodyBox>
+    </BodyContainer>
+  )
+}
+
+export default NavBody
